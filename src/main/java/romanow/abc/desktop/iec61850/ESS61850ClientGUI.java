bd3dae9eb94351275044ae30f285c6d8419e0c26
romanow-nsk <romanow@ngs.ru>
2022-12-15 17:23:04 +0700

    61850 Эксперименты

---
+++ src/main/java/romanow/abc/desktop/iec61850/ESS61850ClientGUI.java
@@ -0,0 +1,9 @@
+package romanow.abc.desktop.iec61850;
+
+import org.openmuc.openiec61850.clientgui.ClientGui;
+
+public class ESS61850ClientGUI {
+    public static void main(String ss[]){
+        new ClientGui().setVisible(true);
+    }
+}
