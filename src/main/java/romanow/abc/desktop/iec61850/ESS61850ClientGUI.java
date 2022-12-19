de424ba273ee4539bea075a94175fe96b9590ae4
Романов <romanow@ngs.ru>
2022-12-19 17:33:44 +0700

    61850 Обновление данных в сервере

--- src/main/java/romanow/abc/desktop/iec61850/ESS61850ClientGUI.java
+++ src/main/java/romanow/abc/desktop/iec61850/ESS61850ClientGUI.java
@@ -4,6 +4,7 @@ import org.openmuc.openiec61850.clientgui.ClientGui;
 
 public class ESS61850ClientGUI {
     public static void main(String ss[]){
+
         new ClientGui().setVisible(true);
     }
 }
