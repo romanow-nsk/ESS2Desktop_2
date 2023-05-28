4fe87c9ea2cb315bea1d358f68a9c6e854810c52
romanow-nsk <romanow@ngs.ru>
2023-05-28 19:52:16 +0700

    Период=0 - нет опроса

--- ess24567.bat
+++ ess24567.bat
@@ -1,1 +1,1 @@
-java -cp ESS2Desktop.jar romanow.abc.dataserver.ESSConsoleServer port:4567 conf:ESS2 iec61850:0
+java -cp ESS2Desktop.jar romanow.abc.dataserver.ESSConsoleServer port:4567 conf:ESS2 dbase:MongoDB3.0 iec61850:0
