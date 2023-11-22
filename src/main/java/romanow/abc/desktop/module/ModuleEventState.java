8df34cb0c8cc67f1513d9f67f325e7da834fd3ff
romanow-nsk <romanow@ngs.ru>
2023-11-22 22:31:09 +0700

    80 - Сообщение МУ по прямому CallBack

--- src/main/java/romanow/abc/desktop/module/ModuleEventState.java
+++ src/main/java/romanow/abc/desktop/module/ModuleEventState.java
@@ -5,8 +5,8 @@ import romanow.abc.core.constants.Values;
 public class ModuleEventState extends ModuleEventAll {
     public ModuleEventState(){ super(false);}
     public boolean typeFilter(int type) {
-        return type== Values.EventState || type==Values.EventDEStateReg || type==Values.EventDEBitReg;
+        return type== Values.EventState || type==Values.EventDEStateReg || type==Values.EventDEBitReg || type==Values.EventPLC;
         }
-    private int[] eTypes = {Values.EventState, Values.EventDEStateReg, Values.EventDEBitReg};
+    private int[] eTypes = {Values.EventState, Values.EventDEStateReg, Values.EventDEBitReg,Values.EventPLC};
     public int[] eventTypes(){ return eTypes; }
 }
