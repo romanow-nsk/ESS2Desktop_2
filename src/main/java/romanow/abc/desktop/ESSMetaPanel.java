2152704c25a3e339bf4f7a2228af07eee7d2f9ed
romanow-nsk <romanow@ngs.ru>
2024-03-14 15:06:03 +0700

    86.01 - смещение часового пояса

--- src/main/java/romanow/abc/desktop/ESSMetaPanel.java
+++ src/main/java/romanow/abc/desktop/ESSMetaPanel.java
@@ -2172,7 +2172,7 @@ public class ESSMetaPanel extends ESSBasePanel {
                     metaXML.testLocalConfiguration();
                     System.out.println(metaXML.getErrors());
                     int timeZoneHour = main.getClientContext().workSettings().getTimeZoneHours();
-                    String zz = WizardBaseView.openWizard(metaFile.getFile().getRef().createArtifactFileName(-timeZoneHour),main, metaXML, new I_Value<String>() {
+                    String zz = WizardBaseView.openWizard(metaFile.getFile().getRef().createArtifactFileName(timeZoneHour),main, metaXML, new I_Value<String>() {
                         @Override
                         public void onEnter(String value) {
                             changesCount++;
