0e65ff07d43c116d472feba6db8cb12f9730e8bd
romanow-nsk <romanow@ngs.ru>
2024-03-29 16:10:47 +0700

    87.01 - Парсинг snmp (предварительный)

--- bars4569bg.bat
+++
@@ -1,1 +0,0 @@
-nohup java -cp ESS2Desktop.jar romanow.abc.dataserver.ESSConsoleServer port:4569 conf:BARS iec61850:0 &
--- bars4569import.bat
+++
@@ -1,1 +0,0 @@
-java -cp ESS2Desktop.jar romanow.abc.dataserver.ESSConsoleServer port:4569 conf:BARS import:bars4569.xls iec61850:0
