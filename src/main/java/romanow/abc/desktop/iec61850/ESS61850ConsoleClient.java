0eb9b43dde4d9a0f68dfc11ff6104457ed42e202
romanow-nsk <romanow@ngs.ru>
2024-04-18 18:20:02 +0700

    88.04 Замена библиотеки + модуль исходника

--- src/main/java/romanow/abc/desktop/iec61850/ESS61850ConsoleClient.java
+++ src/main/java/romanow/abc/desktop/iec61850/ESS61850ConsoleClient.java
@@ -1,7 +1,7 @@
 package romanow.abc.desktop.iec61850;
 
-
-import org.openmuc.openiec61850.app.ConsoleClient;
+//import org.openmuc.openiec61850.app.ConsoleClient;
+import com.beanit.iec61850bean.app.ConsoleClient;
 
 public class ESS61850ConsoleClient {
     public static void main(String ss[]){
